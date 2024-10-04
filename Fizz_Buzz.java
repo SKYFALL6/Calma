7847a6c9b93618212205d6b9316d054052181116
SKYFALL6 <danialexi2019@gmail.com>
2024-10-04 13:34:54 -0500

    le coloque rayitas

--- Fizz_Buzz.java
+++ Fizz_Buzz.java
@@ -6,16 +6,23 @@ public class Fizz_Buzz {
             if (i % 3 == 0 && i % 5 == 0){
 
             System.out.println("fizzbuzz");
+            System.out.println("------------------------");
+            
 
             }else if(i % 5 == 0){
 
                 System.out.println("buzz");
+                System.out.println("------------------------");
 
             }else if (i % 3 == 0){
 
-                System.out.println("fizz");}
+                System.out.println("fizz");
+                System.out.println("------------------------");}
+               
 
                 else {System.out.println(i);
+                    System.out.println("------------------------");
+                    
             }
 
         }
